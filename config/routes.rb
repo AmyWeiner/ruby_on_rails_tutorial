4e9ae1a6bfea05b9ce88580ac5cfd17504463d81
Amy Weiner <arweiner@alumni.stanford.edu>
2014-04-02 15:44:23 -0700

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root "static_pages#home"
   get "/help", to: "static_pages#help", as: "help"
   get "/about", to: "static_pages#about", as: "about"
   get "/contact", to: "static_pages#contact", as: "contact"
   get "/signup", to: "users#new", as: "signup"
+  get "/signin", to: "sessions#new", as: "signin"
+  delete "/signout", to: "sessions#destroy", as: "signout"
 end
